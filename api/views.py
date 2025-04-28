4fa3e1b15ced327c8483d25dae39bd0fbbf7b74f
RafaMadera18 <rafa_madlop@hotmail.com>
2025-04-28 17:14:30 -0600

    DP-4: Vista Vehiculo

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# views.py
+from rest_framework import generics
+from .models import Vehiculo
+from .serializers import VehiculoSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class VehiculoCreateView(generics.CreateAPIView):
+    queryset = Vehiculo.objects.all()
+    serializer_class = VehiculoSerializer
+    permission_classes = [IsAuthenticated]
