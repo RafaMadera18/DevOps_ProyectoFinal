511bf2be736ee8cb05d124641eeda00939d3f227
RafaMadera18 <rafa_madlop@hotmail.com>
2025-04-28 13:30:07 -0600

    DP-2-#12. Cambios Jenkins

--- app/settings.py
+++ app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--u_!lk!l8!ku!s$kd)iecbma^l2*xu-1=)otw(v!ddduhhc!fr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
