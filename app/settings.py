d453e2ec11663757c6225bb01d423c3dc751020f
RafaMadera18 <rafa_madlop@hotmail.com>
2025-04-27 18:10:20 -0600

    Revert "DP-2-#3: Configuracion BD MySQL"
    This reverts commit 4acb761b879bac001aeabf54c0afd2cd253fbec3.

--- app/settings.py
+++ app/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api'
 ]
 
 MIDDLEWARE = [
@@ -76,12 +74,8 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME', 'nombre_base_datos'),
-        'USER': os.environ.get('DB_USER', 'usuario_django'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'contraseña_django'),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
