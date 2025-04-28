e091d57fc64c0706bd762b34c1b88b9e4d0ecea5
RafaMadera18 <rafa_madlop@hotmail.com>
2025-04-28 17:11:40 -0600

    DP-4: Clase Vehiculo

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Vehiculo(models.Model):
+    marca = models.CharField(max_length=100)
+    modelo = models.CharField(max_length=100)
+    vin = models.CharField(max_length=100, unique=True)
+    placa = models.CharField(max_length=50, unique=True)
+    fecha_compra = models.DateField()
+    costo = models.DecimalField(max_digits=10, decimal_places=2)
+    fotografia = models.ImageField(upload_to='vehiculos_fotos/')
+    fecha_ingreso = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.marca} {self.modelo} - {self.placa}'
