a8269f426733f7e9ece3ee8119c8f44a5f6ad80d
RafaMadera18 <rafa_madlop@hotmail.com>
2025-04-29 10:19:33 -0600

    DP-4: Cambiar DefaultRouter

--- api/urls.py
+++ api/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 from rest_framework import routers
 from api import views
 
-router=routers.DefaultRouter
-router.register(r'vehiculos', views.VehiculoViewSet, 'vehiculos')
+router = routers.DefaultRouter()
+router.register(r'vehiculos', views.VehiculoViewSet, basename='vehiculos')
 
-urlpatterns=[
-    path('', include(router.urls))
+urlpatterns = [
+    path('', include(router.urls)),
 ]
