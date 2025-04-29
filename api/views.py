bb855f44e16f59e0fb4c1858e42189eb52c7103a
RafaMadera18 <rafa_madlop@hotmail.com>
2025-04-29 10:06:43 -0600

    DP-4: Añadir Vistas vehiculo

--- api/views.py
+++ api/views.py
@@ -1,10 +1,8 @@
 # views.py
-from rest_framework import generics
+from rest_framework import viewsets
 from .models import Vehiculo
 from .serializers import VehiculoSerializer
-from rest_framework.permissions import IsAuthenticated
 
-class VehiculoCreateView(generics.CreateAPIView):
+class VehiculoViewSet(viewsets.ModelViewSet):
     queryset = Vehiculo.objects.all()
     serializer_class = VehiculoSerializer
-    permission_classes = [IsAuthenticated]
